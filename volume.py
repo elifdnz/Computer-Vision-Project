6f911af0c5514c952779b2807d161f9709836f0d
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-14 10:48:15 +0300

    add hand detection

--- volume.py
+++ volume.py
@@ -1,11 +1,27 @@
 import cv2
-import numpy as np 
+import mediapipe as mp
 
-vid = cv2.VideoCapture(0)
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.7)
+
+cap = cv2.VideoCapture(0)
 
 while True:
-    _ , img = vid.read()
+    success, image = cap.read()
 
-    cv2.imshow("Picture",img)
-    cv2.waitKey(5)
-    cv2.destroyAllWindows()
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(image)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow("Hands", image)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
