d0c2599ba3f59fdb17c136a7b9be26fef84a6b99
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-17 10:24:59 +0300

    changed file

--- faceattandece/attandence.py
+++ faceattandece/attandence.py
@@ -6,15 +6,15 @@ import os
 
 path = 'images'
 imList = os.listdir(path)
-
+'''
 imgKivanc = face_recognition.load_image_file('images/kivanc.jpeg')
 imgKivanc = cv2.resize(imgKivanc,(640,480))
 imgKivanc = cv2.cvtColor(imgKivanc,cv2.COLOR_BGR2RGB)
 
-imgtest= face_recognition.load_image_file('images/test.jpeg')
+imgtest= face_recognition.load_image_file('images/kivan.jpeg')
 imgtest = cv2.resize(imgtest,(640,480))
 imgtest = cv2.cvtColor(imgtest,cv2.COLOR_BGR2RGB)
-
+'''
 arrIm = []
 arrName = []
 
