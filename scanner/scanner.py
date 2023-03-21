5fa2c79dc58d85e318cdc7d32d592bdbea99f997
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-21 09:59:04 +0300

    add apply threshold code

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,31 +1,28 @@
 import cv2
 import numpy as np
+import utlis
 
-img = cv2.imread('scaner.jpeg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+scn = cv2.imread('scaner.jpeg')
+cap = cv2.VideoCapture(0)
+web = True
+cap.set(10,160)
+height = 640
+width = 480
 
-# Kenar tespiti
-edges = cv2.Canny(gray, 40, 150, apertureSize=3)
+count = 0
 
-# Hough çizgi tespiti 
-lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=50, maxLineGap=10)
+utlis.initializeTrackbars()
+while True:
+    imgB = np.zeros((height,width,3),np.uint8)
+    if web :succes,img = cap.read()
+    else: img = cap.read(scn)
+    img = cv2.resize(img,(width,height))
+    imgG = cv2.cvtColor(img,cv2.COLOR_BAYER_BG2GRAY)
+    imgBlu = cv2.GaussianBlur(imgG,(5,5),1)
+    thrs=utlis.valTrackbars()
+    imgThreshold = cv2.Canny(imgBlu,thrs[0],thrs[1])
+    kernel = np.ones((5,5))
+    imgDial = cv2.dilate(imgThreshold,kernel,iterations=2)
+    imgThreshold = cv2.erode(imgDial,kernel,iterations=1)
+    
 
-# Dikdörtgenleri oluştur
-rectangles = []
-for line in lines:
-    x1, y1, x2, y2 = line[0]
-    angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
-    length = np.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
-    if length > 50 and (angle > -30 and angle < 30 or angle > 60 and angle < 120 or angle < -150 or angle > 150):
-        rectangles.append(line[0])
-        
-# Dikdörtgenleri çizdir
-for rect in rectangles:
-    x1, y1, x2, y2 = rect
-    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-# Sonucu göster
-cv2.imwrite('./cikti.jpeg', img)
-#cv2.imshow('image', img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
