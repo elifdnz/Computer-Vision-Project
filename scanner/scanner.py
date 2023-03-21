c2f47c6ed71f7c95ae77e503c4d5bbc861b49e22
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-21 10:35:18 +0300

    add label on the image

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -13,24 +13,24 @@ count = 0
 
 utlis.initializeTrackbars()
 while True:
-    imgB = np.zeros((height,width,3),np.uint8)
-    if web :succes,img = cap.read()
-    else: img = cap.read(scn)
-    img = cv2.resize(img,(width,height))
-    imgG = cv2.cvtColor(img,cv2.COLOR_BAYER_BG2GRAY)
-    imgBlu = cv2.GaussianBlur(imgG,(5,5),1)
-    thrs=utlis.valTrackbars()
-    imgThreshold = cv2.Canny(imgBlu,thrs[0],thrs[1])
-    kernel = np.ones((5,5))
-    imgDial = cv2.dilate(imgThreshold,kernel,iterations=2)
-    imgThreshold = cv2.erode(imgDial,kernel,iterations=1)
+    img = cv2.resize(img,(width,height)) # RESIZE IMAGE
+    imgBlank = np.zeros((height,width, 3), np.uint8) # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # CONVERT IMAGE TO GRAY SCALE
+    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1) # ADD GAUSSIAN BLUR
+    thres= [200,200]
+    imgThreshold = cv2.Canny(imgBlur,thres[0],thres[1]) # APPLY CANNY BLUR
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2) # APPLY DILATION
+    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
 
-    imgContour = img.copy()
-    imgBigContour = img.copy()
-    contour, hiyearchy = cv2.findContours(imgThreshold,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(imgContour,contour,-1,(0,255,0),10)
+    ## FIND ALL COUNTOURS
+    imgContours = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    imgBigContour = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # FIND CONTOURS
+    cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS
 
-    biggest, maxArea = utlis.biggestContour(contour) # FIND THE BIGGEST CONTOUR
+    # FIND THE BIGGEST COUNTOUR
+    biggest, maxArea = utlis.biggestContour(contours) # FIND THE BIGGEST CONTOUR
     if biggest.size != 0:
         biggest=utlis.reorder(biggest)
         cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
@@ -40,6 +40,7 @@ while True:
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
         imgWarpColored = cv2.warpPerspective(img, matrix, (width, height))
 
+
         imgWarpColored=imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
         imgWarpColored = cv2.resize(imgWarpColored,(width,height))
 
@@ -47,3 +48,28 @@ while True:
         imgAdaptiveThre= cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
         imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
         imgAdaptiveThre=cv2.medianBlur(imgAdaptiveThre,3)
+
+        imageArray = ([img,imgGray,imgThreshold,imgContours],
+                      [imgBigContour,imgWarpColored, imgWarpGray,imgAdaptiveThre])
+ 
+    else:
+        imageArray = ([img,imgGray,imgThreshold,imgContours],
+                      [imgBlank, imgBlank, imgBlank, imgBlank])
+ 
+    # LABELS FOR DISPLAY
+    lables = [["Original","Gray","Threshold","Contours"],
+              ["Biggest Contour","Warp Prespective","Warp Gray","Adaptive Threshold"]]
+ 
+    stackedImage = utlis.stackImages(imageArray,0.75,lables)
+    cv2.imshow("Result",stackedImage)
+ 
+    # SAVE IMAGE WHEN 's' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgWarpColored)
+        cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
+                      (1100, 350), (0, 255, 0), cv2.FILLED)
+        cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
+                    cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+        cv2.imshow('Result', stackedImage)
+        cv2.waitKey(300)
+        count += 1
